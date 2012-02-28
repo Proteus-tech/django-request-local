6861f1dd6a4599f18c909ae49a1c689e4c198d3a
Pam Suebvisai <sinapam@proteus-tech.com>
2012-02-28 19:20:32 +0700

    add setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='django-request-local',
+    version='0.1',
+    description='Make request object available throughout the current thread',
+    author='Proteus Technologies Co. Ltd.',
+    author_email='team@proteus-tech.com',
+    url='https://github.com/Proteus-tech/django-request-local',
+    packages=['django_request_local'])
+  
